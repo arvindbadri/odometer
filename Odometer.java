55410f7539c5f1a9e27587d04f4f246ff965a6ea
Arvind <abadrinath@paypalcorp.com>
2016-08-03 17:20:20 +0530

    Created file with functions that do nothing

---
+++ Odometer.java
@@ -0,0 +1,31 @@
+public class Odometer {
+    int currentReading;
+
+    static int getSmallestReading (int numberOfDigits) {
+        return 0;
+    }
+
+    static int getLargestReading (int numberOfDigits) {
+        return 0;
+    }
+
+    static int getNextReading (int reading) {
+        return 0;
+    }
+
+    static int getPreviousReading (int reading) {
+        return 0;
+    }
+
+    static int getDistance (int firstReading, int secondReading) {
+        return 0;
+    }
+
+    static int getNReadingsAfter (int reading, int n) {
+        return 0;
+    }
+
+    static int getNReadingsBefore (int reading, int n) {
+        return 0;
+    }
+}
